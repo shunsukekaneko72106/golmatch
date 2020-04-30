3c5c3b6275a80ab2e43419880fa2cc627c1713c0
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2020-04-30 17:42:56 +0900

    デプロイ準備

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update, :index]
+  resources :coaches, only: [:index,:new]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
-  resources :coaches, only: [:index,:new]
 end
